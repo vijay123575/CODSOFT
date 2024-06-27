7ac279a3d3c8309c71e52f4be3a21d9a99d19144
vijay123575 <malkarivijaykumar80@gmail.com>
2024-06-27 11:24:14 +0530

    Add files via upload
    created random password generator using python.

---
+++ # python programme to create a Random Password.py
@@ -0,0 +1,17 @@
+# python programme to create a Random Password Generator
+import random
+import string
+print("Welcome to our Random Password Generator")
+def main():
+
+    length=int(input("Enter the length of password you want: "))
+    lowerD=string.ascii_lowercase
+    upperD=string.ascii_uppercase
+    digitD=string.digits
+    symbolsD=string.punctuation
+    combine=lowerD+upperD+digitD+symbolsD
+    x=random.sample(combine,length)
+    password="".join(x)
+    print(password)
+    main()
+main()    
